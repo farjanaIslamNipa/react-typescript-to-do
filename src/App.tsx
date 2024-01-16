d83a229281d09e9eed83d6ba8f3192434b6e024b
farjanaIslamNipa <nipa.farzana87@gmail.com>
2024-01-16 17:59:17 +0600

    modal design completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
+import {createContext, useState} from "react";
+import Modal from "./components/ui/Modal";
+
+export const ModalContext = createContext(null)
+
 const App = () => {
+  const [modal, setModal] = useState(false);
+  const [taskList, setTaskList] = useState([]);
+
+  const handleModal = () => {
+    setModal(prev => !prev)
+  }
+
+
   return (
+  <ModalContext.Provider value={{taskList, setTaskList}}>
     <div className="w-full max-w-[800px] mx-auto pt-10">
       <div className="flex justify-between items-center bg-white rounded-2xl rounded-b-none px-8 py-5">
         <h2 className="text-2xl font-bold text-gray-700">Task List</h2>
-        <button className="bg-green-600 px-4 py-2 rounded-md text-white flex justify-center items-center">
+        <button onClick={handleModal} className="bg-green-600 px-4 py-2 rounded-md text-white flex justify-center items-center">
           <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 24 24" fill="none">
-          <path d="M7 12L12 12M12 12L17 12M12 12V7M12 12L12 17" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M7 12L12 12M12 12L17 12M12 12V7M12 12L12 17" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
           <span className="inline-block pr-1 bg-green-600">Add Task</span>
         </button>
+        {
+          modal && 
+          (<Modal isOpen={modal} onClose={handleModal}></Modal>)
+        }
       </div>
       <div className="mt-5 bg-white rounded-2xl rounded-t-none p-5">
-        <p className="text-center pt-5 text-lg text-gray-400 font-medium">Task list is empty <button className="underline text-green-600 text-base px-1">Add Task</button></p>
+        <p className="text-center pt-5 text-lg text-gray-400 font-medium">Task list is empty 
+        <button onClick={handleModal} className="underline text-green-600 text-base px-1">Add Task</button></p>
         <div className="w-full flex justify-center items-center opacity-20">
           <svg xmlns="http://www.w3.org/2000/svg" width="300px" height="300px" viewBox="0 0 24 24" fill="none">
           <path d="M15 18.5L20 13.5M20 18.5L15 13.5" stroke="#1C274C" strokeWidth="1.3" strokeLinecap="round" strokeLinejoin="round"/>
@@ -23,6 +42,7 @@ const App = () => {
         </div>
       </div>
     </div>
+  </ModalContext.Provider>
   );
 };
 
---
+++ src/components/ui/Modal.tsx
@@ -0,0 +1,79 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {createPortal} from "react-dom";
+import cn from "../../utils/cn";
+import {MouseEvent, ReactNode, useContext, useRef} from "react";
+import {ModalContext} from "../../App";
+
+type TModal = {
+  isOpen : boolean;
+  onClose: () => void;
+  // children: ReactNode
+}
+const Modal = ({isOpen, onClose} : TModal) => {
+
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const closeModalFromOutside = (e: MouseEvent) => {
+    if(!containerRef.current?.contains(e.target as Node)) {
+      onClose()
+    }
+  }
+
+  const {taskList, setTaskList} = useContext(ModalContext)
+
+  const addTask = (e: any) => {
+    e.preventDefault()
+    setTaskList(prev => prev.push('new task'))
+    console.log(taskList, 'task')
+  }
+
+
+
+  // console.log(taskList, 'task')
+
+  return createPortal(
+    <div onClick={closeModalFromOutside} className={cn("flex justify-center items-center fixed inset-0 bg-gray-800/40 invisible z-[999]",{visible: isOpen})}>
+      <div ref={containerRef} className="bg-white w-full max-w-[500px] px-10 py-10 rounded-2xl">
+        <div className="flex justify-between items-center pb-9">
+          <h3 className="text-2xl text-gray-600 font-semibold">Add new task</h3>
+          <button onClick={onClose}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 24 24" fill="none">
+            <rect width="24" height="24" fill="white"/>
+            <path d="M7 17L16.8995 7.10051" stroke="#000000" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M7 7.00001L16.8995 16.8995" stroke="#000000" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
+          </button>
+        </div>
+        <div className="pb-2 text-gray-600">
+          <form onSubmit={addTask}>
+            <div className="mb-6">
+              <input type="text" placeholder="Enter task name" className="w-full border border-gray-300 rounded-md px-3 py-2" />
+            </div>
+            <div className="px-2">
+              {/* <p className="block text-base font-medium mb-3">Select Priority</p> */}
+              <div className="flex items-center gap-x-8">
+                <div className="flex items-center gap-1">
+                  <input type="radio" name="priority" id="high" className="block border border-gray-500 rounded-full checked:bg-green-500 checked:border-green-500 text-green-500 focus:ring-1 focus:ring-green-500" />
+                  <label htmlFor="high" className="font-medium text-sm">High</label>
+                </div>
+                <div className="flex items-center gap-1">
+                  <input type="radio" name="priority" id="medium" className="block border border-gray-500 rounded-full checked:bg-green-500 checked:border-green-500 text-green-500 focus:ring-1 focus:ring-green-500" />
+                  <label htmlFor="medium" className="font-medium text-sm">Medium</label>
+                </div>
+                <div className="flex items-center gap-1">
+                  <input type="radio" name="priority" id="low" className="block border border-gray-500 rounded-full checked:bg-green-500 checked:border-green-500 text-green-500 focus:ring-1 focus:ring-green-500" />
+                  <label htmlFor="low" className="font-medium text-sm">Low</label>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-end mt-8">
+              <button type="submit" className="bg-green-600 px-4 py-2 rounded-md text-white">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>, document.getElementById('portal') as Element
+  );
+};
+
+export default Modal;
